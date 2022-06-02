a74081e86c5d1c076f8d036f0e48676f6a433b5f
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2022-06-03 00:26:46 +0800

    More chaotic fixes ahead
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- scripts/get-writefreely-binary.sh
+++ scripts/get-writefreely-binary.sh
@@ -9,9 +9,9 @@ fi
 ACCESS_KEY="1DXWofMPgzcjWYy2b7F9HYxWAXMNDVDroAWQBV5i7t3fn784Gay3ekM8TgZfFrGeHo6JanUuNA8ukS84jH5XzV1sY73v1HPWcBDQDUFtM8AbuVGjRHEhNQSWFHNWE8LTRvuW57XBLTyCy5w4ndR7ntkKBBxq4v4hPaX2K8jvFE8WwzTzu5c9wncd4LmKGUSjDBtZ3j4M4EWp81xgZjhpdkoLSpLxKSJQLxL63UL8SHF9xEoiH2knf87udwXjMzVxyLsngT2sSp8ZRCjgkupHSL1JisDJdE96bEbddECnAhUFhoHwUEcuA5RPRntn55HNoGEYkwZjerjR8wxKD2eNj7"
 BASE_URL="https://link.eu1.storjshare.io/s"
 BUCKET_PATH="recaptime-file-dumps/ajhalili2006/writefreely"
-VERSION=0.13.1
+VERSION="0.13.1"
 
-if command -v wget; then
+if command -v wget >> /dev/null; then
   wget "$BASE_URL/$ACCESS_KEY/$BUCKET_PATH/writefreely_$VERSION_linux_amd64.tar.gz?download=1" -O /tmp/writefreely-release.tar.gz
 else
   curl "$BASE_URL/$ACCESS_KEY/$BUCKET_PATH/writefreely_$VERSION_linux_amd64.tar.gz?download=1" -o /tmp/writefreely-release.tar.gz
